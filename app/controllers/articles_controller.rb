f45547c5858f4ba366e9f471982eee2cbf01c090
furuyaatsushi <sdvxmnn@gmail.com>
2020-01-01 01:59:00 +0900

    controller if else

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,12 +10,13 @@ class ArticlesController < ApplicationController
   end
 
   def create
-
     @article = Article.new(article_params)
     @article.save
 
     if @article.save
       redirect_to root_path
+    else
+      redirect_to new_article_path
     end
   end
 
