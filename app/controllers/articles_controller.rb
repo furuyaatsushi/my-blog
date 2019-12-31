d608588e18838998e22518bafc6c458ded0fcd59
furuyaatsushi <sdvxmnn@gmail.com>
2019-12-31 22:08:46 +0900

    form image

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,11 +10,18 @@ class ArticlesController < ApplicationController
 
   def create
 
+    @article = Article.new(article_params)
+    binding.pry
+    @article.save
+
+    if @article.save
+      redirect_to root_path
+    end
   end
 
   private
   def article_params
-    params.requier(:article).permit(:title, :content, images_attributes: [:id, :content] ).merge(user_id: current_user.id)
+    params.require(:article).permit(:title, :content, images_attributes: [:id, :content] ).merge(user_id: current_user.id)
   end
 
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,4 @@
 class Image < ApplicationRecord
-  mount_uploader :content, ImagesUploader
-
   belongs_to :article
+  mount_uploader :content, ImagesUploader
 end
