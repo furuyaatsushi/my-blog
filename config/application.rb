3cb41ba4716f34a763b1a110d729ecee6622cf84
furuyaatsushi <sdvxmnn@gmail.com>
2020-01-04 23:26:19 +0900

    add ja.yml

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module MyBlog
       g.test_framework false
     end
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
   end
 end
