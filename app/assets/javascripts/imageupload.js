089a4eb3864dcc9b6fdbdde7b19c819f661ac7ef
furuyaatsushi <sdvxmnn@gmail.com>
2020-01-05 21:23:35 +0900

    upload image 4

--- app/assets/javascripts/imageupload.js
+++ app/assets/javascripts/imageupload.js
@@ -10,4 +10,9 @@ $(document).on('turbolinks:load', function(){
     var new_input1 = $(`<input name="article[images_attributes][2][content]", type="file", id="article_images_attributes_2_content">`);
     input_area.prepend(new_input1);
   })
+
+  $(document).on('change', '#article_images_attributes_2_content', function(){
+    var new_input2 = $(`<input name="article[images_attributes][3][content]", type="file", id="article_images_attributes_3_content">`);
+    input_area.prepend(new_input2);
+  })
 })
