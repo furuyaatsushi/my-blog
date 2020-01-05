3de7f37e83259c1678e27e2aca7d3c777d0c232a
furuyaatsushi <sdvxmnn@gmail.com>
2020-01-05 21:07:41 +0900

    js change append to prepend

--- app/assets/javascripts/imageupload.js
+++ app/assets/javascripts/imageupload.js
@@ -3,11 +3,11 @@ $(document).on('turbolinks:load', function(){
 
   $(document).on('change', '#article_images_attributes_0_content', function(){
     var new_input = $(`<input name="article[images_attributes][1][content]", type="file", id="article_images_attributes_1_content">`);
-    input_area.append(new_input);
+    input_area.prepend(new_input);
   })
 
   $(document).on('change', '#article_images_attributes_1_content', function(){
     var new_input1 = $(`<input name="article[images_attributes][2][content]", type="file", id="article_images_attributes_2_content">`);
-    input_area.append(new_input1);
+    input_area.prepend(new_input1);
   })
 })
