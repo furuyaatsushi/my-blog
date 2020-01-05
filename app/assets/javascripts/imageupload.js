282b4cf7627bed64a9e1b337bac167e94d93fab9
furuyaatsushi <sdvxmnn@gmail.com>
2020-01-05 22:36:36 +0900

    add class file field

--- app/assets/javascripts/imageupload.js
+++ app/assets/javascripts/imageupload.js
@@ -2,22 +2,22 @@ $(document).on('turbolinks:load', function(){
   var input_area = $('.upload-box');
 
   $(document).on('change', '#article_images_attributes_0_content', function(){
-    var new_input = $(`<input name="article[images_attributes][1][content]", type="file", id="article_images_attributes_1_content">`);
+    var new_input = $(`<input name="article[images_attributes][1][content]", type="file", id="article_images_attributes_1_content", class="post-img">`);
     input_area.prepend(new_input);
   })
 
   $(document).on('change', '#article_images_attributes_1_content', function(){
-    var new_input1 = $(`<input name="article[images_attributes][2][content]", type="file", id="article_images_attributes_2_content">`);
+    var new_input1 = $(`<input name="article[images_attributes][2][content]", type="file", id="article_images_attributes_2_content", class="post-img">`);
     input_area.prepend(new_input1);
   })
 
   $(document).on('change', '#article_images_attributes_2_content', function(){
-    var new_input2 = $(`<input name="article[images_attributes][3][content]", type="file", id="article_images_attributes_3_content">`);
+    var new_input2 = $(`<input name="article[images_attributes][3][content]", type="file", id="article_images_attributes_3_content", class="post-img">`);
     input_area.prepend(new_input2);
   })
 
   $(document).on('change', '#article_images_attributes_3_content', function(){
-    var new_input3 = $(`<input name="article[images_attributes][4][content]", type="file", id="article_images_attributes_4_content">`);
+    var new_input3 = $(`<input name="article[images_attributes][4][content]", type="file", id="article_images_attributes_4_content", class="post-img">`);
     input_area.prepend(new_input3);
   })
 })
