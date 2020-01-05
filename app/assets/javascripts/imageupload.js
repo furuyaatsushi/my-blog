b92ff2ded0fdc7ad266ad2ee6213bee737116949
furuyaatsushi <sdvxmnn@gmail.com>
2020-01-06 01:50:09 +0900

    js if

--- app/assets/javascripts/imageupload.js
+++ app/assets/javascripts/imageupload.js
@@ -1,10 +1,15 @@
 $(document).on('turbolinks:load', function(){
   var input_area = $('.upload-box');
 
-  $(document).on('change', '#article_images_attributes_0_content', function(){
-    var new_input = $(`<input name="article[images_attributes][1][content]", type="file", id="article_images_attributes_1_content", class="post-img">`);
-    input_area.prepend(new_input);
-  })
+  var inputFirst = document.getElementById("article_images_attributes_1_content")
+  console.log(inputFirst)
+
+  if (inputFirst == null ){
+    $(document).on('change', '#article_images_attributes_0_content', function(){
+      var new_input = $(`<input name="article[images_attributes][1][content]", type="file", id="article_images_attributes_1_content", class="post-img">`);
+      input_area.prepend(new_input);
+    })
+  }
 
   $(document).on('change', '#article_images_attributes_1_content', function(){
     var new_input1 = $(`<input name="article[images_attributes][2][content]", type="file", id="article_images_attributes_2_content", class="post-img">`);
