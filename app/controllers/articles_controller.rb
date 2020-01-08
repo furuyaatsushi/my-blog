31ff6a11782527f4815336a5d0df4e970223a094
furuyaatsushi <sdvxmnn@gmail.com>
2020-01-09 03:06:22 +0900

    comment order

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,8 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+
+    @comments = @article.comments.order('updated_at DESC')
   end
 
   def new
