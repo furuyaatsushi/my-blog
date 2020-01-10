0cf5b7161f609ec306ccca7f5279772fd5fc91ae
furuyaatsushi <sdvxmnn@gmail.com>
2020-01-10 23:49:00 +0900

    set favorite routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'articles#index'
   resources :articles do
     resources :comments, only: [:create]
+    resources :likes, only: [:create, :destroy]
   end
   resources :users
 end
