71319c049c976e4e577e55c68c92f7b5a8852c66
furuyaatsushi <sdvxmnn@gmail.com>
2020-01-11 01:09:56 +0900

    model method

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,8 @@ class User < ApplicationRecord
   has_many :comments
   has_many :favorites, dependent: :destroy
   has_many :favorited_articles, through: :favorites, source: :article
+
+  def already_liked?(post)
+    self.likes.exists?(post_id: post.id)
+  end
 end
