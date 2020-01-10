a2d187b5360d3f387f5b02d365a8892745f7c9d3
furuyaatsushi <sdvxmnn@gmail.com>
2020-01-10 23:58:44 +0900

    destroy action

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,4 +5,9 @@ class FavoritesController < ApplicationController
     redirect_back(fallback_location: root_path)
   end
 
+  def destroy
+    @favorite = Favorite.find_by(article_id: params[:article_id], user_id: current_user.id)
+    @favorite.destroy
+    redirect_back(fallback_location: root_path)
+  end
 end
