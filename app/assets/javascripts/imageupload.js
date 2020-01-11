292ae361a50617f6d9ec6226a1fbc70a483a7a1a
furuyaatsushi <sdvxmnn@gmail.com>
2020-01-12 01:58:49 +0900

    image upload box

--- app/assets/javascripts/imageupload.js
+++ app/assets/javascripts/imageupload.js
@@ -3,32 +3,33 @@ $(document).on('turbolinks:load', function(){
 
   var inputFirst = document.getElementById("article_images_attributes_1_content")
 
-  if (inputFirst == null ){
-    $(document).on('change', '#article_images_attributes_0_content', function(){
-      var new_input = $(`<input name="article[images_attributes][1][content]", type="file", id="article_images_attributes_1_content", class="post-img">`);
-      input_area.prepend(new_input);
-    })
-  }
+  // if (inputFirst == null ){
+  //   $(document).on('change', '#article_images_attributes_0_content', function(){
+  //     var new_input = $(`<input name="article[images_attributes][1][content]", type="file", id="article_images_attributes_1_content", class="post-img">`);
+  //     input_area.prepend(new_input);
+  //   })
+  // }
 
-  $(document).on('change', '#article_images_attributes_1_content', function(){
-    var new_input1 = $(`<input name="article[images_attributes][2][content]", type="file", id="article_images_attributes_2_content", class="post-img">`);
-    input_area.prepend(new_input1);
-  })
+  // $(document).on('change', '#article_images_attributes_1_content', function(){
+  //   var new_input1 = $(`<input name="article[images_attributes][2][content]", type="file", id="article_images_attributes_2_content", class="post-img">`);
+  //   input_area.prepend(new_input1);
+  // })
 
-  $(document).on('change', '#article_images_attributes_2_content', function(){
-    var new_input2 = $(`<input name="article[images_attributes][3][content]", type="file", id="article_images_attributes_3_content", class="post-img">`);
-    input_area.prepend(new_input2);
-  })
+  // $(document).on('change', '#article_images_attributes_2_content', function(){
+  //   var new_input2 = $(`<input name="article[images_attributes][3][content]", type="file", id="article_images_attributes_3_content", class="post-img">`);
+  //   input_area.prepend(new_input2);
+  // })
 
-  $(document).on('change', '#article_images_attributes_3_content', function(){
-    var new_input3 = $(`<input name="article[images_attributes][4][content]", type="file", id="article_images_attributes_4_content", class="post-img">`);
-    input_area.prepend(new_input3);
-  })
+  // $(document).on('change', '#article_images_attributes_3_content', function(){
+  //   var new_input3 = $(`<input name="article[images_attributes][4][content]", type="file", id="article_images_attributes_4_content", class="post-img">`);
+  //   input_area.prepend(new_input3);
+  // })
 
 
   $(document).on('change', '.post-img',function() {
-    $('.upload-box').children(":first").css({'display':'block'});
-    $(this).css({'display':'none'});
+    $('.upload-box2').css({'display':'block'});
+    $(this.parentNode).css({'display':'none'});
+    $('.uploaded-box').css({'display':'block'});
   })
     
 })
