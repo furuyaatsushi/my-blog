fdd741fe70454963b99cbca2ed202c61dd9d69a4
furuyaatsushi <sdvxmnn@gmail.com>
2020-01-11 23:20:05 +0900

    small fix

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module MyBlog
     end
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+    config.time_zone = 'Tokyo'
   end
 end
