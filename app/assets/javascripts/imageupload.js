9985bb382869243f9e83d7c800f299dbe91d4ef0
furuyaatsushi <sdvxmnn@gmail.com>
2020-01-12 15:20:33 +0900

    preview image4

--- app/assets/javascripts/imageupload.js
+++ app/assets/javascripts/imageupload.js
@@ -28,6 +28,7 @@ $(document).on('turbolinks:load', function(){
   var filefield = $('#article_images_attributes_0_content')
   var filefield1 = $('#article_images_attributes_1_content')
   var filefield2 = $('#article_images_attributes_2_content')
+  var filefield3 = $('#article_images_attributes_3_content')
 
   $(filefield).on('change', filefield, function(e) {
     $('#upload-box2').css({'display':'block'});
@@ -86,6 +87,30 @@ $(document).on('turbolinks:load', function(){
     file = e.target.files[0]
     reader = new FileReader(),
     preview = $('#preview2');
+    preview.empty();
+
+    reader.onload = (function(file) {
+      return function(e) {
+        preview.empty();
+        preview.append($('<img>').attr({
+          src: e.target.result,
+          height: "100%",
+          class: "preview",
+          title: file.name
+        }));
+      };
+    })(file);
+    reader.readAsDataURL(file);
+  });
+
+  $(filefield3).on('change', filefield3, function(e) {
+    $('#upload-box5').css({'display':'block'});
+    $(this.parentNode).css({'display':'none'});
+    $('#uploaded-box3').css({'display':'block'});
+
+    file = e.target.files[0]
+    reader = new FileReader(),
+    preview = $('#preview3');
     preview.empty();
 
     reader.onload = (function(file) {
