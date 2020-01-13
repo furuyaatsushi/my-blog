ca7a43a82b13476b45b9b863a9aa655cef208e21
furuyaatsushi <sdvxmnn@gmail.com>
2020-01-13 22:40:25 +0900

    user page pagenation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @articles = @user.articles.order('updated_at DESC').includes(:images)
+    @articles = @user.articles.order('updated_at DESC').includes(:images).page(params[:page]).per(5)
   end
 end
