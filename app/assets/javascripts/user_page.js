c24ab28e75f047e7627e1e75438ff615113865e6
furuyaatsushi <sdvxmnn@gmail.com>
2020-01-15 20:30:03 +0900

    tab move

--- app/assets/javascripts/user_page.js
+++ app/assets/javascripts/user_page.js
@@ -9,4 +9,14 @@ $(document).on('turbolinks:load', function(){
     element.classList.remove('active');
     element2.classList.add('active');
   })
+
+  $('.posts-list-head').on('click', function(){
+    $('.user-posted').css({'display':'block'});
+    $('.user-favorites').css({'display':'none'});
+
+    var element = document.getElementById('user-post-article');
+    var element2 = document.getElementById('user-favorites');
+    element2.classList.remove('active');
+    element.classList.add('active');
+  })
 })
