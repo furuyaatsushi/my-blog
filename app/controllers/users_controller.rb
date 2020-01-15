8ca2f1c44709b652d1a7ed90a7514fefca935970
furuyaatsushi <sdvxmnn@gmail.com>
2020-01-15 19:58:48 +0900

    create favorite tab content

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @articles = @user.articles.order('updated_at DESC').includes(:images).page(params[:page]).per(5)
+    @favorites = @user.favorited_articles.order('updated_at DESC').includes(:images).page(params[:page]).per(5)
   end
 
   def favorites
