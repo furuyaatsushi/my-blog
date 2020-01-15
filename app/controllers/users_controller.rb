b02d3c5fb6d48593e926113a905e5570a186684a
furuyaatsushi <sdvxmnn@gmail.com>
2020-01-15 19:26:24 +0900

    add side menu favorite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :authenticate_user!, only: [:show, :favorites]
+
   def show
     @user = User.find(params[:id])
     @articles = @user.articles.order('updated_at DESC').includes(:images).page(params[:page]).per(5)
