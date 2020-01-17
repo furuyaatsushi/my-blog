38f8a7449613ae63fdd347d286d1d37340fd63d0
furuyaatsushi <sdvxmnn@gmail.com>
2020-01-17 23:24:25 +0900

    create tags table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200110140015) do
+ActiveRecord::Schema.define(version: 20200117142104) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title",                    null: false
@@ -46,6 +46,12 @@ ActiveRecord::Schema.define(version: 20200110140015) do
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
     t.index ["article_id"], name: "index_images_on_article_id", using: :btree
+  end
+
+  create_table "tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
