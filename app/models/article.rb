85abd8aa59cbe4e0fb3cce77ba9d6d6fea5a7aab
furuyaatsushi <sdvxmnn@gmail.com>
2020-01-18 00:48:01 +0900

    create tag

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,7 +6,7 @@ class Article < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorited_users, through: :fovorite, source: :user
   has_many :article_tags, dependent: :destroy
-  has_many :tags, through: :article_tag
+  has_many :tags, through: :article_tags
 
   accepts_nested_attributes_for :images
 
@@ -18,15 +18,15 @@ class Article < ApplicationRecord
 
   def save_articles(savearticle_tags)
     current_tags = self.tags.pluck(:name) unless self.tags.nil?
-    old_tags = current_tags - savepost_tags
-    new_tags = savepost_tags - current_tags
+    old_tags = current_tags - savearticle_tags
+    new_tags = savearticle_tags - current_tags
 
     old_tags.each do |old_name|
       self.tags.delete Tag.find_by(name:old_name)
     end
     new_tags.each do |new_name|
       post_tag = Tag.find_or_create_by(name:new_name)
-      self.tags << post_tag
+      self.tags << article_tags
     end
   end
 end
