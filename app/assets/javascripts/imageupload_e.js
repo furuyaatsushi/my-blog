4eb95e7f56f1647a5635dd0867b813c4c5f42237
furuyaatsushi <sdvxmnn@gmail.com>
2020-01-19 16:24:10 +0900

    if preview 2 upload box

--- app/assets/javascripts/imageupload_e.js
+++ app/assets/javascripts/imageupload_e.js
@@ -3,8 +3,13 @@ $(document).on('turbolinks:load', function(){
   $(document).ready(function(){
     if(
       document.getElementById("previewed0") != null
-      || document.getElementById("previewed0") == null){
+      && document.getElementById("previewed1") == null){
       $('#upload-box2').css({'display':'block'});
+    }else if(
+      document.getElementById("previewed0") != null
+      && document.getElementById("previewed1") != null
+      && document.getElementById("previewed2") == null){
+      $('#upload-box3').css({'display':'block'});
     };
   })
 });
